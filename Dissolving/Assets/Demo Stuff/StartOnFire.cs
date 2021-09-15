589f6289f1209e4c111c9b96230a906cae06cfbe
Cole <49996960+CWoulf@users.noreply.github.com>
2021-09-15 14:17:53 -0500

    trying stuff

--- Dissolving/Assets/Demo Stuff/StartOnFire.cs
+++ Dissolving/Assets/Demo Stuff/StartOnFire.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class StartOnFire : MonoBehaviour
 {
     [SerializeField] Material mat;
-    [SerializeField] Material defaultmat;
     [SerializeField] ParticleSystem fire;
     bool isOnFire = false;
-    
+
+    private void Start()
+    {
+        //StartCoroutine(StartFire());
+    }
+
+    IEnumerator StartFire()
+    {
+        fire.Play();
+        yield return new WaitForSeconds(3f);
+        fire.Clear();
+        fire.Pause();
+    }
 
     void Update()
     {
+        /*
         if(Input.GetKey(KeyCode.Space) && !fire.isPlaying)
         {
             fire.Play();
-            Debug.Log("Burn Mat");
-            GetComponent<MeshRenderer>().material = mat;
         }
         else if (fire.isPlaying && Input.GetKey(KeyCode.Space))
         {
             fire.Clear();
             fire.Pause();
         }
+        */
+        //StartCoroutine(StartFire());
     }
     
 }
